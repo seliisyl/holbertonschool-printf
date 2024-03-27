e4e8924700e49b1aa62527aefd8ac3db14f56a38
paulineonier <pauline.onier@outlook.fr>
2024-03-27 13:17:47 +0000

    adding file for c

---
+++ print-c.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_c - writes the character c to stdout
+ *@args: variablre number of parameters
+ *Return: on success 1
+*/
+
+int print_c(va_list args)
+{
+
+	char str;
+
+	str = va_arg(args, int);
+	_putchar(str);
+	return (1);
+}
