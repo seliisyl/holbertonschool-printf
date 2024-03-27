e5883ad0c5e745180108e59d4e26311df1a4639b
paulineonier <pauline.onier@outlook.fr>
2024-03-27 13:18:08 +0000

    adding file for s

---
+++ print-s.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_s - prints string to stdout
+ * @args: arguments
+ * Return: 0 on success
+ */
+
+int print_s(va_list args)
+{
+	int i = 0;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
