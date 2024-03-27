1bc6c4a7e369eabcde802cd07b0414151433dba2
paulineonier <pauline.onier@outlook.fr>
2024-03-27 13:17:05 +0000

    adding file for %

---
+++ print-%.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_purcent - print the purcent sign
+ * @args : list of arguments
+ * Return: our little purcent specifier
+ */
+
+int print_purcent(__attribute__((unused)) va_list args)
+{
+	return (_putchar('%'));
+}
